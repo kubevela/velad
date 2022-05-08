df1bb6d8ececa791cd664fc22924eb9813df3cb7
qiaozp <chivalry.pp@gmail.com>
2022-05-08 17:09:07 +0800

    Fix: try fix staticcheck
    Signed-off-by: qiaozp <chivalry.pp@gmail.com>

--- makefiles/dependency.mk
+++ makefiles/dependency.mk
@@ -32,7 +32,7 @@ ifeq (, $(shell which staticcheck))
 	@{ \
 	set -e ;\
 	echo 'installing honnef.co/go/tools/cmd/staticcheck ' ;\
-	GO111MODULE=on go get honnef.co/go/tools/cmd/staticcheck@v0.3.0 ;\
+	go install honnef.co/go/tools/cmd/staticcheck@v0.3.0 ;\
 	}
 STATICCHECK=$(GOBIN)/staticcheck
 else
