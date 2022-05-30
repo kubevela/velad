0395f05b3573873f202fdb3d7f8650fb0ef57e47
qiaozp <chivalry.pp@gmail.com>
2022-05-30 21:26:21 +0800

    Feat: automate the image tag detection

--- hack/download_vela_images.sh
+++ hack/download_vela_images.sh
@@ -24,16 +24,37 @@ else
   velaux_version=v$2
 fi
 
-vela_images=("oamdev/vela-core:${vela_version}"
-  "oamdev/cluster-gateway:v1.3.2"
-  "oamdev/kube-webhook-certgen:v2.3"
-  "oamdev/velaux:${velaux_version}"
-  "oamdev/vela-apiserver:${vela_version}")
+function set_cluster_gateway_version() {
+    cluster_gateway_version=UNKNOWN
+    image_tag=$(cat pkg/resources/static/vela/charts/vela-core/values.yaml | grep -A 1 oamdev/cluster-gateway | grep tag)
+    cluster_gateway_version=$(echo $image_tag| cut -f2 -d:|xargs)
+    echo "cluster-gateway image version detected:" $cluster_gateway_version
+}
 
-for IMG in ${vela_images[*]}; do
-  IMAGE_NAME=$(echo "$IMG" | cut -f1 -d: | cut -f2 -d/)
-  echo saving "$IMG" to "$VELA_IMAGE_DIR"/"$IMAGE_NAME".tar.gz
-  docker pull "$IMG"
-  docker save -o "$VELA_IMAGE_DIR"/"$IMAGE_NAME".tar "$IMG"
-  gzip -f "$VELA_IMAGE_DIR"/"$IMAGE_NAME".tar
-done
+function set_certgen_version() {
+    certgen_version=UNKNOWN
+    image_tag=$(cat pkg/resources/static/vela/charts/vela-core/values.yaml | grep -A 1 oamdev/kube-webhook-certgen | grep tag)
+    certgen_version=$(echo $image_tag| cut -f2 -d:|xargs)
+    echo "kube-webhook-certgen image version detected:" $certgen_version
+}
+
+function download_images() {
+  vela_images=("oamdev/vela-core:${vela_version}"
+    "oamdev/cluster-gateway:${cluster_gateway_version}"
+    "oamdev/kube-webhook-certgen:${certgen_version}"
+    "oamdev/velaux:${velaux_version}"
+    "oamdev/vela-apiserver:${vela_version}")
+
+  for IMG in ${vela_images[*]};
+    do
+      IMAGE_NAME=$(echo "$IMG" | cut -f1 -d: | cut -f2 -d/)
+      echo saving "$IMG" to "$VELA_IMAGE_DIR"/"$IMAGE_NAME".tar.gz
+      docker pull "$IMG"
+      docker save -o "$VELA_IMAGE_DIR"/"$IMAGE_NAME".tar "$IMG"
+      gzip -f "$VELA_IMAGE_DIR"/"$IMAGE_NAME".tar
+    done
+}
+
+set_cluster_gateway_version
+set_certgen_version
+download_images
